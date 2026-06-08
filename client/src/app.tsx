285040a879f4e1b4d051286cd6934b35dc52b1ea
Nicholas Willmott <nicholaswillmottse@gmail.com>
2026-06-08 17:19:45 +0800

    Deploy version 1.0.5 at 2026_06_08_17_19_45

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,6 +1,6 @@
 import { Router, Route } from "@solidjs/router";
 import { Suspense } from "solid-js";
-import { AlertProvider } from "panther";
+import { AlertProvider, PopoverMenuProvider } from "panther";
 import "./app.css";
 import CentralLoggedInWrapper from "./routes/index";
 
@@ -8,6 +8,7 @@ export default function App() {
   return (
     <>
       <AlertProvider />
+      <PopoverMenuProvider />
       <Router root={(p) => <Suspense>{p.children}</Suspense>}>
         <Route path="/*" component={CentralLoggedInWrapper} />
       </Router>
